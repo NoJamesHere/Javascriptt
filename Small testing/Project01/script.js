d0ff5fd63490f1b38f2280b19a0ccc1b3db4f196
NoJamesHere <davidedwin2007@gmail.com>
2025-07-28 21:31:33 +0200

    little update

--- Small testing/Project01/script.js
+++ Small testing/Project01/script.js
@@ -1,11 +1,9 @@
 
 function clicked() {
     const texty = document.getElementById("texty");
-    let text = texty.textContent;
-    texty.textContent = "You clicked the button!"
+
     texty.classList.add("animate");
     setTimeout(() => {
-        texty.textContent = text;
         texty.classList.remove("animate");
 
     }, 800);
