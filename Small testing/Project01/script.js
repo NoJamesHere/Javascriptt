2b68051efff84fe4e3e2ae33b534d7b6224a4df4
NoJamesHere <davidedwin2007@gmail.com>
2025-07-29 13:35:28 +0200

    Added a.. 'mysterious circle' of some sort

--- Small testing/Project01/script.js
+++ Small testing/Project01/script.js
@@ -8,3 +8,17 @@ function clicked() {
 
     }, 800);
 }
+
+function something() {
+    let x = Math.random() * 90; // keep within bounds
+    let y = Math.random() * 90;
+    const divy = document.getElementById("Circle");
+
+    divy.style.left = x + "%";
+    divy.style.top = y + "%";
+    divy.classList.add("animatecircle"); 
+    setTimeout(() => {
+        
+        divy.classList.remove("animatecircle");
+    }, 800);
+}
