db5ad74de44b6a2650b17802556ae9d0320fc348
NoJamesHere <davidedwin2007@gmail.com>
2025-08-26 18:45:00 +0200

    New Project!

--- lang.js
+++ lang.js
@@ -10,6 +10,8 @@ const translations = {
         proj3Desc: "Some button designs.",
         proj4Link: "Project 4",
         proj4Desc: "A javascript game inspired by the infamous \"snake\" game.",
+        proj5Link: "Project 5",
+        proj5Desc: "I don't know yet!",
         footerText: "&copy; 2025 NoJamesHere &mdash; <a href=\"https://github.com/NoJamesHere\" target=\"_blank\" id=\"github-link\">GitHub</a>"
     },
     de: {
@@ -23,6 +25,8 @@ const translations = {
         proj3Desc: "Einige Button-Designs.",
         proj4Link: "Projekt 4",
         proj4Desc: "Ein JavaScript-Spiel inspiriert vom berühmten \"Snake\"-Spiel.",
+        proj5Link: "Projekt 5",
+        proj5Desc: "Ich weiß es noch nicht!",
         footerText: "&copy; 2025 NoJamesHere &mdash; <a href=\"https://github.com/NoJamesHere\" target=\"_blank\" id=\"github-link\">GitHub</a>"
     },
     ru: {
@@ -36,6 +40,8 @@ const translations = {
         proj3Desc: "Несколько дизайнов кнопок.",
         proj4Link: "Проект 4",
         proj4Desc: "Игра на JavaScript, вдохновленная знаменитой игрой \"Змейка\".",
+        proj5Link: "Проект 5",
+        proj5Desc: "Niet!",
         footerText: "&copy; 2025 NoJamesHere &mdash; <a href=\"https://github.com/NoJamesHere\" target=\"_blank\" id=\"github-link\">GitHub</a>"
     }
 };
@@ -44,6 +50,8 @@ document.getElementById('lang-select').addEventListener('change', function() {
     const lang = this.value;
     document.getElementById('main-title').textContent = translations[lang].mainTitle;
     document.getElementById('main-desc').textContent = translations[lang].mainDesc;
+    
+    // Project lists
     document.getElementById('proj1-link').textContent = translations[lang].proj1Link;
     document.getElementById('proj1-desc').textContent = translations[lang].proj1Desc;
     document.getElementById('proj2-link').textContent = translations[lang].proj2Link;
@@ -52,5 +60,9 @@ document.getElementById('lang-select').addEventListener('change', function() {
     document.getElementById('proj3-desc').textContent = translations[lang].proj3Desc;
     document.getElementById('proj4-link').textContent = translations[lang].proj4Link;
     document.getElementById('proj4-desc').textContent = translations[lang].proj4Desc;
+    document.getElementById('proj5-link').textContent = translations[lang].proj5Link;
+    document.getElementById('proj5-desc').textContent = translations[lang].proj5Desc;
+    
+    // Footer
     document.getElementById('footer-text').innerHTML = translations[lang].footerText;
 });
