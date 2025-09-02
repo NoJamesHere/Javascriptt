a5d42d714d71f23d2a26ae8a6faa0ce2788327b6
NoJamesHere <davidedwin2007@gmail.com>
2025-09-02 16:48:44 +0200

    Did a bit of polishing

--- Small testing/Project06/script.js
+++ Small testing/Project06/script.js
@@ -1,17 +1,19 @@
 const header = document.getElementById("header");
 const paragraph = document.getElementById("paragraph");
 const background = document.getElementById("background");
-
+const imageslider = document.getElementById("imageslider");
 const colors = {
   dark: {
     header_color: "#7867b6ff",
     paragraph_color: "#827acbff",
     background_color: "#2c2c4eff",
+    imageslider: "moonDarkMode.png",
   },
   light: {
     header_color: "#342a48ff",
     paragraph_color: "#433e60ff",
     background_color: "#c5c1ffff",
+    imageslider: "sunLightMode.png",
   },
 };
 
@@ -19,6 +21,7 @@ function changeTheme(theme) {
   header.style.color = colors[theme].header_color;
   paragraph.style.color = colors[theme].paragraph_color;
   background.style.backgroundColor = colors[theme].background_color;
+  imageslider.src = colors[theme].imageslider;
 }
 
 changeTheme("light");
